924f95857797bd8a227411416ba824e2778b81df
liorwebdemos <liorwebdemos@gmail.com>
2024-08-21 22:17:58 +0300

    bugfix

--- WebApi/Models/Entities/Repo.cs
+++ WebApi/Models/Entities/Repo.cs
@@ -1,5 +1,3 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace WebApi.Models.Models
 {
 	/// <summary>GitHub repository<para />
@@ -10,7 +8,6 @@ namespace WebApi.Models.Models
 		public string Name { get; set; } = string.Empty;
 
 		/// <summary></summary>
-		[RegularExpression(@"^[a-zA-Z0-9]+/[a-zA-Z0-9]+$", ErrorMessage = "The value must be in the format: letters and/or numbers, followed by a '/', followed by more letters and/or numbers.")]
 		public string FullName { get; set; } = string.Empty;
 
 		/// <summary></summary>
