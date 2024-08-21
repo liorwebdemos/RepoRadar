f4d96b65906cb4f6587bd058691f3dea80f4c065
liorwebdemos <liorwebdemos@gmail.com>
2024-08-21 18:07:10 +0300

    remove angular material and animations

--- ClientPages/src/app/app.config.ts
+++ ClientPages/src/app/app.config.ts
@@ -5,10 +5,8 @@ import {
 } from "@angular/core";
 import { provideRouter } from "@angular/router";
 import { routes } from "./app.routes";
-import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
 import { provideHttpClient, withInterceptors } from "@angular/common/http";
 import { authInterceptor } from "./auth.interceptor";
-import { authGuard } from "./auth.guard";
 
 export const API_BASE_URL = new InjectionToken<string>("API_BASE_URL");
 
@@ -16,7 +14,6 @@ export const appConfig: ApplicationConfig = {
 	providers: [
 		provideZoneChangeDetection({ eventCoalescing: true }),
 		provideRouter(routes),
-		provideAnimationsAsync(),
 		provideHttpClient(withInterceptors([authInterceptor])),
 		{ provide: API_BASE_URL, useValue: "https://localhost:7076/api/" }, // should put value(s) in environment configuration files
 	],
